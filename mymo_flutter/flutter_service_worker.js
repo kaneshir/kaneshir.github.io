5629dcdb893fa71fc16a4e62afc1bccae5ce3cdb
zimmerd <derek.kaneshiro@zimmerbiomet.com>
2020-04-26 17:38:59 -0700

    update

--- mymo_flutter/flutter_service_worker.js
+++ mymo_flutter/flutter_service_worker.js
@@ -1,18 +1,23 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"assets/FontManifest.json": "f7161631e25fbd47f3180eae84053a51",
+  "assets/AssetManifest.json": "149da7ebca59cd9019410d3694d3c0f0",
+"assets/assets/images/logo_poweredby_ZB.png": "c43f17e41c2275f95a0391b07ba262c2",
+"assets/assets/images/mymobility_LoginLogo.png": "fbf2235634e0b5e98cc37d57cccea67a",
+"assets/FontManifest.json": "18eda8e36dfa64f14878d07846d6e17f",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"assets/LICENSE": "6d8bcab152d5cc91be05babf5159afe1",
+"assets/LICENSE": "6a947e762c873a085db123ae9e350830",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "5a37ae808cf9f652198acde612b5328d",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "2bca5ec802e40d3f4b60343e346cedde",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "2aa350bd2aeab88b601a593f793734c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "0cfd9d273b7996a9c39aa64afc208931",
-"/": "0cfd9d273b7996a9c39aa64afc208931",
-"main.dart.js": "485ae5e38badd569d7708575f126ef92",
-"manifest.json": "f625cf9395a76d3ea6efd5713833e05e"
+"index.html": "724526fdc28338ce3ed3296ef2cc0625",
+"/": "724526fdc28338ce3ed3296ef2cc0625",
+"main.dart.js": "088b062f756ac4a3eb02e839e3d3111b",
+"manifest.json": "7869911b3cbcec6e49893c589c6e0022"
 };
 
 self.addEventListener('activate', function (event) {
